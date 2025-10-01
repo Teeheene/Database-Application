291c7c2007af5778d17ef437ded4ae37565bb2b9
Teeheene <diane.panganiban1010@gmail.com>
2025-10-01 12:35:49 +0000

    chore: clean project structure

--- lrun.sh
+++ lrun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Create the output directory if it doesn't exist
-mkdir -p classes
+mkdir -p src/classes
 
 # Compile Java files into the classes/ folder
-javac -Xmaxerrs 10000 -Xmaxwarns 500 -Xlint -d classes *.java model/*.java view/*.java
+javac -Xmaxerrs 10000 -Xmaxwarns 500 -Xlint -d src/classes src/*.java src/model/*.java src/view/*.java src/controller/*.java
 
 # Run Main from the classes folder
 if [ $? -eq 0 ]; then
-   java -cp classes Main 
+   java -cp src/classes Main 
 fi
 
