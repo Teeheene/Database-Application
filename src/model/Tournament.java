2216c3c2c24defb7cb75cf012f58d95278455bbe
Wesley Hong <wesley_hong@dlsu.edu.ph>
2025-10-31 21:07:47 +0800

    tournament code draft

---
+++ src/model/Tournament.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * draft
+ */
+public class Tournament {
+
+    private int tournamentID;
+    private String tournamentName;
+    private int seasonYear;
+    private String tournamentType;
+    private String startDate;
+    private String endDate;
+    private Timestamp teamBracket;
+    private Timestamp fanFavoriteTeam;
+
+}
