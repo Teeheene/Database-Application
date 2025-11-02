e55da86ec267528296350d0ffaf6ea298b5a7d53
Teeheene <diane.panganiban1010@gmail.com>
2025-11-02 16:26:47 +0800

    chore: fix format

--- src/db/basketball_app.sql
+++ src/db/basketball_app.sql
@@ -4,37 +4,37 @@ USE basketball_app;
 DROP TABLE IF EXISTS enthusiast;
 CREATE TABLE enthusiast (
 	enthusiast_id INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(25) NOT NULL UNIQUE,
-    lastname VARCHAR(50) NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    middlename VARCHAR(50),
-    sex VARCHAR(10) NOT NULL,
-    date_of_birth TIMESTAMP NOT NULL,
-    hashed_password INT, 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	username VARCHAR(25) NOT NULL UNIQUE,
+   lastname VARCHAR(50) NOT NULL,
+   firstname VARCHAR(50) NOT NULL,
+   middlename VARCHAR(50),
+   sex VARCHAR(10) NOT NULL,
+   date_of_birth TIMESTAMP NOT NULL,
+	hashed_password INT, 
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (enthusiast_id)
 );
 
 DROP TABLE IF EXISTS engagement;
 CREATE TABLE engagement (
 	engagement_id INT AUTO_INCREMENT,
-    engagement_category VARCHAR(10) NOT NULL,
-    engagement_type VARCHAR(10) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    enthusiast_id INT,
-    PRIMARY KEY (engagement_id),
-    FOREIGN KEY (enthusiast_id) REFERENCES enthusiast(enthusiast_id)
+   engagement_category VARCHAR(10) NOT NULL,
+   engagement_type VARCHAR(10) NOT NULL,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   enthusiast_id INT,
+   PRIMARY KEY (engagement_id),
+   FOREIGN KEY (enthusiast_id) REFERENCES enthusiast(enthusiast_id)
 );
 
 DROP TABLE IF EXISTS player;
 CREATE TABLE player (
 	player_id INT NOT NULL AUTO_INCREMENT,
-    lastname VARCHAR(50) NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    middlename VARCHAR(50),
-    sex VARCHAR(10) NOT NULL,
-    date_of_birth TIMESTAMP NOT NULL,
-    height INT,
+   lastname VARCHAR(50) NOT NULL,
+   firstname VARCHAR(50) NOT NULL,
+   middlename VARCHAR(50),
+   sex VARCHAR(10) NOT NULL,
+   date_of_birth TIMESTAMP NOT NULL,
+   height INT,
 	weight INT,
 	rStatus BOOLEAN,
 	PRIMARY KEY (player_id)
