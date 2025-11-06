3d41ab11c87e489b660ef8ec69079106bc51c21d
dlsu-Lcarlos <luis_guiller_carlos@dlsu.edu.ph>
2025-11-06 20:47:56 +0800

    Refactor basketball_app.sql to update table structure
    Added team transaction and moved engagement to be under transactions

--- src/db/basketball_app.sql
+++ src/db/basketball_app.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS basketball_app;
 USE basketball_app;
-
+-- CORE RECORDS
 DROP TABLE IF EXISTS enthusiast;
 CREATE TABLE enthusiast (
 	enthusiast_id INT NOT NULL AUTO_INCREMENT,
@@ -15,17 +15,6 @@ CREATE TABLE enthusiast (
 	PRIMARY KEY (enthusiast_id)
 );
 
-DROP TABLE IF EXISTS engagement;
-CREATE TABLE engagement (
-	engagement_id INT AUTO_INCREMENT,
-   engagement_category VARCHAR(10) NOT NULL,
-   engagement_type VARCHAR(10) NOT NULL,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   enthusiast_id INT,
-   PRIMARY KEY (engagement_id),
-   FOREIGN KEY (enthusiast_id) REFERENCES enthusiast(enthusiast_id)
-);
-
 DROP TABLE IF EXISTS player;
 CREATE TABLE player (
 	player_id INT NOT NULL AUTO_INCREMENT,
@@ -37,5 +26,31 @@ CREATE TABLE player (
    height INT,
 	weight INT,
 	rStatus BOOLEAN,
-	PRIMARY KEY (player_id)
+    team_id INT,
+	PRIMARY KEY (player_id),
+    FOREIGN KEY (team_id) REFERENCES team(team_ID) ON DELETE SET NULL
 );
+
+-- TRANSACTION RECORDS
+DROP TABLE IF EXISTS team;
+CREATE TABLE team (
+   team_ID INT AUTO_INCREMENT,
+    team_name VARCHAR(50) NOT NULL,
+    -- coach_id INT, (for when its added)
+    registry_status BOOLEAN,
+    tCreated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY(team_ID)
+ -- FOREIGN KEY(coach_id) REFERENCES coach(coach_id)
+);
+
+DROP TABLE IF EXISTS engagement;
+CREATE TABLE engagement (
+	engagement_id INT AUTO_INCREMENT,
+   engagement_category VARCHAR(10) NOT NULL,
+   engagement_type VARCHAR(10) NOT NULL,
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   enthusiast_id INT,
+   PRIMARY KEY (engagement_id),
+   FOREIGN KEY (enthusiast_id) REFERENCES enthusiast(enthusiast_id)
+);
+
