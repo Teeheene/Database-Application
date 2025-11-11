78fe8f6d7d706967eb63584ed733edcdb195fbea
Teeheene <diane.panganiban1010@gmail.com>
2025-11-11 13:36:53 +0000

    feat: implement db connection helper

--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -1,3 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-	private static final String URL = ""
+	//adjust accordingly
+	private static final String URL = "jdbc:mysql://localhost:3306/basketball_app";
+	private static final String USER = "root";
+	private static final String PASSWORD = "password";
+
+	public static Connection getConnection() {
+		try {
+			return DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}	
 }
