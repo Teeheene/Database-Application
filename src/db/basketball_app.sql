604770f26d4f1384cb2a7b94ee8ea1dee9629859
Teeheene <diane.panganiban1010@gmail.com>
2025-11-11 19:35:45 +0000

    chore: fix db format

--- src/db/basketball_app.sql
+++ src/db/basketball_app.sql
@@ -6,6 +6,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS enthusiast;
 DROP TABLE IF EXISTS engagement;
 DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS tournaments;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -13,11 +14,11 @@ DROP TABLE IF EXISTS enthusiast;
 CREATE TABLE enthusiast (
 	enthusiast_id INT NOT NULL AUTO_INCREMENT,
 	username VARCHAR(25) NOT NULL UNIQUE,
-   lastname VARCHAR(50) NOT NULL,
-   firstname VARCHAR(50) NOT NULL,
-   middlename VARCHAR(50),
-   sex VARCHAR(10) NOT NULL,
-   date_of_birth DATE NOT NULL,
+	lastname VARCHAR(50) NOT NULL,
+	firstname VARCHAR(50) NOT NULL,
+	middlename VARCHAR(50),
+	sex VARCHAR(10) NOT NULL,
+	date_of_birth DATE NOT NULL,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (enthusiast_id)
 );
@@ -25,23 +26,23 @@ CREATE TABLE enthusiast (
 DROP TABLE IF EXISTS engagement;
 CREATE TABLE engagement (
 	engagement_id INT AUTO_INCREMENT,
-   engagement_category VARCHAR(10) NOT NULL,
-   engagement_type VARCHAR(10) NOT NULL,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   enthusiast_id INT,
-   PRIMARY KEY (engagement_id),
-   FOREIGN KEY (enthusiast_id) REFERENCES enthusiast(enthusiast_id)
+	engagement_category VARCHAR(10) NOT NULL,
+	engagement_type VARCHAR(10) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	enthusiast_id INT,
+	PRIMARY KEY (engagement_id),
+	FOREIGN KEY (enthusiast_id) REFERENCES enthusiast(enthusiast_id)
 );
 
 DROP TABLE IF EXISTS player;
 CREATE TABLE player (
 	player_id INT NOT NULL AUTO_INCREMENT,
-   lastname VARCHAR(50) NOT NULL,
-   firstname VARCHAR(50) NOT NULL,
-   middlename VARCHAR(50),
-   sex VARCHAR(10) NOT NULL,
-   date_of_birth TIMESTAMP NOT NULL,
-   height INT,
+	lastname VARCHAR(50) NOT NULL,
+	firstname VARCHAR(50) NOT NULL,
+	middlename VARCHAR(50),
+	sex VARCHAR(10) NOT NULL,
+	date_of_birth DATE NOT NULL,
+    height INT,
 	weight INT,
 	rStatus BOOLEAN,
 	PRIMARY KEY (player_id)
@@ -54,7 +55,7 @@ CREATE TABLE coach (
 	firstname VARCHAR(50) NOT NULL,
 	middlename VARCHAR(50),
 	sex VARCHAR(10) NOT NULL,
-	date_of_birth TIMESTAMP NOT NULL,
+	date_of_birth DATE NOT NULL,
 	start_year INT NOT NULL,
 	end_year INT,
 	years_in_field INT NOT NULL,
@@ -62,13 +63,12 @@ CREATE TABLE coach (
 	PRIMARY KEY (coach_id)
 );
 
+DROP TABLE IF EXISTS tournaments;
 CREATE TABLE tournaments (
-  tournament_id INT AUTO_INCREMENT PRIMARY KEY,
-  tournament_name VARCHAR(200) NOT NULL,
-  season_year INT,
-  tournament_type VARCHAR(50),
-  start_date DATE,
-  end_date DATE,
-  team_bracket VARCHAR(255),
-  fan_favorite_team_id INT NULL
+	tournament_id INT AUTO_INCREMENT PRIMARY KEY,	
+    tournament_name VARCHAR(200) NOT NULL,
+	season_year INT,
+	tournament_type VARCHAR(50),
+	start_date DATE,
+	end_date DATE
 );
