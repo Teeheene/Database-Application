9b6e88eff374c04fbadc6c0c8085bc2eb987f98b
Wesley-Hong <wesley_hong@dlsu.edu.ph>
2025-11-15 01:15:17 +0800

    class template for game and game bracket / management

---
+++ src/model/GameBracket.java
@@ -0,0 +1,8 @@
+package model;
+
+import util.*;
+import java.util.*;
+
+public class GameBracket {
+
+}
---
+++ src/model/GameManagement.java
@@ -0,0 +1,14 @@
+package model;
+
+import util.*;
+import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class GameManagement {
+
+}
