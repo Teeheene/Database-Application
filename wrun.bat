b7bdad71b7ec51d1b7a8c3434d7e4c064a10aaa9
Teeheene <diane.panganiban1010@gmail.com>
2025-11-16 06:36:25 +0800

    chore: update bat file for windows

--- wrun.bat
+++ wrun.bat
@@ -1,14 +1,17 @@
 @echo off
-REM Create classes folder if it doesn't exist
-if not exist classes mkdir classes
+REM Create the output directory if it doesn't exist
+if not exist "src\classes" mkdir "src\classes"
 
-REM Compile all .java files into classes\
-javac -d classes *.java controller\*.java model\*.java view\*.java
-if %errorlevel% neq 0 (
-    echo Compilation failed.
-    exit /b %errorlevel%
+REM Path to connector
+set JAR_PATH=lib\mysql-connector-j-9.4.0\mysql-connector-j-9.4.0.jar
+
+REM Compiling
+javac -Xmaxerrs 10000 -Xmaxwarns 500 -Xlint ^
+    -cp "%JAR_PATH%;src\classes" ^
+    -d src\classes ^
+    src\*.java src\model\*.java src\view\*.java src\controller\*.java src\util\*.java
+
+REM Run main if compiling is successful
+if %errorlevel%==0 (
+    java -cp "%JAR_PATH%;src\classes" Main
 )
-
-REM Run Main from the classes folder
-java -cp classes Main
-
