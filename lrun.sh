f5a7e05a494d8c05a88b966f66a4ee62d2757938
Teeheene <diane.panganiban1010@gmail.com>
2025-11-18 06:15:43 +0800

    chore: clean lib and compilation file

--- lrun.sh
+++ lrun.sh
@@ -3,16 +3,20 @@
 mkdir -p src/classes
 
 # Path to connector
-JAR_PATH="lib/mysql-connector-j-9.4.0/mysql-connector-j-9.4.0.jar"
+JAR_PATH="lib/mysql-connector-j-9.4.0.jar"
+OPENPDF_CORE_JAR="lib/openpdf-3.0.1-SNAPSHOT.jar"
+OPENPDF_SWING_JAR="lib/pdf-swing-3.0.1-SNAPSHOT.jar"
+
+CP="$MYSQL_JAR:$OPENPDF_CORE_JAR:$OPENPDF_SWING_JAR:src/classes"
 
 # Compiling
 javac -Xmaxerrs 10000 -Xmaxwarns 500 -Xlint \
-	-cp "$JAR_PATH:src/classes" \
+	-cp "$CP" \
 	-d src/classes \
 	src/*.java src/model/*.java src/view/*.java src/controller/*.java src/util/*.java
 
 # Run main if compiling is successful 
 if [ $? -eq 0 ]; then
-   java -cp "$JAR_PATH:src/classes" Main 
+   java -cp "$CP" Main 
 fi
 
