6e450ed7bb4eec4ac68f804fecc86823aad3ebd1
dlsu-Lcarlos <luis_guiller_carlos@dlsu.edu.ph>
2025-11-18 21:13:03 +0800

    Implement player management methods in AdminController
    Added player management functionality to AdminController.

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.util.LinkedHashMap;
+import java.util.Comparator;
 import java.util.ArrayList;
 import model.*;
 import view.*;
@@ -12,6 +13,7 @@ public class AdminController {
 	private EnthusiastManagement enthusiastModel;
 	private EngagementManagement engagementModel;
     private TournamentManagement tournamentModel;
+	private PlayerManagement playerModel;
 
 	public AdminController() {}
 	public AdminController(GUIView prevView, GUIAdminView view) {
@@ -21,6 +23,7 @@ public class AdminController {
 		enthusiastModel = new EnthusiastManagement();
 		engagementModel = new EngagementManagement();
         tournamentModel = new TournamentManagement();
+		playerModel = new PlayerManagement();
 	}	
 
 	public void handleMenu(String option) {
@@ -29,6 +32,10 @@ public class AdminController {
 				System.out.println("Opening enthusiast dashboard...");
 				view.enthusiastDashboardPanel();
 				break;
+			case "player":
+				System.out.println("Opening player dashboard...");
+				view.playerDashboardPanel();
+			    break;
             case "tournament":
                 System.out.println("Opening tournament dashboard...");
                 view.tournamentDashboardPanel();
@@ -153,6 +160,83 @@ public class AdminController {
 		}
 
 		return data;
+	}
+
+	//PLAYER===========================================
+	public ArrayList<Player> getPlayerInformation() {
+
+		ArrayList<Player> players = new ArrayList<>();
+        for(Player p : playerModel.getAllPlayers()){
+			if(p == null) {continue;}
+			players.add(p);
+		}
+		players.sort(Comparator.comparingInt(Player::getPlayerID));
+		return players;
+	}
+
+	public void addPlayer(Player player) {
+		playerModel.addPlayer(player);
+	}
+
+	public void togglePlayer(Player player) {
+		playerModel.togglePlayer(player.getPlayerID());
+	}
+
+	public void showPlayer(int ID) {
+		Player player = playerModel.searchPlayer(ID);
+		view.viewPlayerPanel(player);
+	}
+
+	public boolean updatePlayerView(int ID) {
+		Player player = playerModel.searchPlayer(ID);
+		if(player == null) return false; 
+		view.updatePlayerPanel(player);
+		return true;
+	}
+	
+	public void updatePlayer(Player updatedPlayer) {
+		Player player = playerModel.updatePlayer(updatedPlayer);
+		view.viewPlayerPanel(player);
+	}
+		
+	public boolean deletePlayerView(int ID) {
+		Player player = playerModel.searchPlayer(ID);
+		if(player == null) return false; 
+		view.deletePlayerPanel(player);
+		return true;
+	}
+	
+	public void deletePlayer(Player player) {
+		playerModel.deletePlayer(player.getPlayerID());
+	}
+
+	public int generatePlayerID() {
+		return playerModel.generatePlayerID();
+	}
+    public void handlePlayer(String option) {
+		switch(option) {
+			case "home":
+				handleMenu("player");
+				break;
+			case "create":
+				view.createPlayerPanel();
+				break;
+			case "viewAll":
+				view.viewAllPlayerPanel(getPlayerInformation());
+				break;
+			case "update":
+				view.searchUpdatePlayerPanel();
+				break;
+			case "delete":
+				view.searchDeletePlayerPanel();
+				break;
+			case "report":
+				view.viewReportPlayerPanel();
+				break;
+			case "exit":
+				view.dashboardPanel();
+				break;
+		}
 	}
 
     // ========= TOURNAMENT============
